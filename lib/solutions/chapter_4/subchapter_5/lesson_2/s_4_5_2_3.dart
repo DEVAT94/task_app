8d70aca93ee48433333e099f23f5a4160e46d7b5
DEVAT94 <al.toth94@gmail.com>
2025-07-15 13:20:51 +0200

    Add Task 4.5.2.4

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
@@ -4,6 +4,33 @@ class S4523 extends StatelessWidget {
   const S4523({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MyNameWidget();
+  }
+}
+
+class MyNameWidget extends StatefulWidget {
+  const MyNameWidget({super.key});
+
+  @override
+  State<MyNameWidget> createState() => _NameState();
+}
+
+class _NameState extends State<MyNameWidget> {
+  String name = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(name),
+        ElevatedButton(
+            onPressed: () {
+              setState(() {
+                name = '$State';
+              });
+            },
+            child: Text('Name anzeigen'))
+      ],
+    );
   }
 }
--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
@@ -4,6 +4,35 @@ class S4524 extends StatelessWidget {
   const S4524({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return ShowHideNameWidget();
+  }
+}
+
+class ShowHideNameWidget extends StatefulWidget {
+  const ShowHideNameWidget({super.key});
+
+  @override
+  State<ShowHideNameWidget> createState() => _ShowHideNameWidgetState();
+}
+
+class _ShowHideNameWidgetState extends State<ShowHideNameWidget> {
+  String name = '';
+  String buttonText = 'Namen anzeigen';
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(name),
+        ElevatedButton(onPressed: () {
+          setState(() {
+            name = name == '' ? 'Alex' : '';
+            buttonText = buttonText == 'Name verstecken' ? 'Name anzeigen' : 'Name verstecken';
+          });
+        }, child: Text(
+          buttonText
+          
+          ))
+      ],
+    );
   }
 }
